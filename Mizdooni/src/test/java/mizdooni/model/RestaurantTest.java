345e49b6b05e71b53bd9af2639d07cd3dbca8a5b
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-10-20 16:56:31 +0330

    INIT: some initiation in RestaurantTest file

--- Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
+++ Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
@@ -1,4 +1,14 @@
 package mizdooni.model;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RestaurantTest {
+    private Restaurant restaurant;
+
+    @BeforeEach
+    public void setup() {
+
+    }
 }

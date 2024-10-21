0452f564dced902ac93ff1169892378e461076a4
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-10-21 17:34:26 +0330

    Refactor TableTest

--- Mizdooni/src/test/java/mizdooni/model/TableTest.java
+++ Mizdooni/src/test/java/mizdooni/model/TableTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class TableTest {
 
     private Table table;
-    private Reservation reservation1;
+    private Reservation reservation;
 
 
     @BeforeEach
@@ -17,33 +20,47 @@ class TableTest {
         ModelTestUtils.addTablesWithDefaultSeatsToRestaurant(restaurant1, 3);
         table = restaurant1.getTable(1);
         User clientUser = ModelTestUtils.getDefaultClientUser();
-        reservation1 = ModelTestUtils.getDefaultReservation(clientUser, restaurant1, table);
-        table.addReservation(reservation1);
+        reservation = spy(ModelTestUtils.getDefaultReservation(clientUser, restaurant1, table));
+        table.addReservation(reservation);
     }
 
     @Test
-    void testAddReservation() {
-        assertEquals(1, table.getReservations().size());
-        assertTrue(table.getReservations().contains(reservation1));
+    void addReservation_EmptyTable_AddsReservation() {
+        assertThat(table.getReservations().size()).isEqualTo(1);
+        assertThat(table.getReservations()).contains(reservation);
     }
 
     @Test
-    void testIsReservedTrue() {
-        LocalDateTime dateTime = reservation1.getDateTime();
-        assertTrue(table.isReserved(dateTime));
+    void isReserved_HasReservation_ReturnsTrue() {
+        LocalDateTime dateTime = ModelTestUtils.DEFAULT_LOCAL_DATE_TIME;
+        when(reservation.getDateTime()).thenReturn(dateTime);
+        when(reservation.isCancelled()).thenReturn(false);
+
+        boolean isReserved = table.isReserved(dateTime);
+
+        assertThat(isReserved).isTrue();
     }
 
     @Test
-    void testIsReservedFalseForDifferentTime() {
-        LocalDateTime dateTime = LocalDateTime.now();
-        assertFalse(table.isReserved(dateTime));
+    void isReserved_DifferentDateTime_ReturnsFalse() {
+        LocalDateTime dateTime = ModelTestUtils.DEFAULT_LOCAL_DATE_TIME;
+        when(reservation.isCancelled()).thenReturn(false);
+        when(reservation.getDateTime()).thenReturn(dateTime.plusDays(1));
+
+        boolean isReserved = table.isReserved(dateTime);
+
+        assertThat(isReserved).isFalse();
     }
 
     @Test
-    void testIsReservedFalseForCancelledReservation() {
-        reservation1.cancel();
-        LocalDateTime dateTime = reservation1.getDateTime();
-        assertFalse(table.isReserved(dateTime));
+    void isReserved_SameDateTimeButCanceled_ReturnsFalse() {
+        LocalDateTime dateTime = ModelTestUtils.DEFAULT_LOCAL_DATE_TIME;
+        when(reservation.isCancelled()).thenReturn(true);
+        when(reservation.getDateTime()).thenReturn(dateTime);
+
+        boolean isReserved = table.isReserved(dateTime);
+
+        assertThat(isReserved).isFalse();
     }
 
 }

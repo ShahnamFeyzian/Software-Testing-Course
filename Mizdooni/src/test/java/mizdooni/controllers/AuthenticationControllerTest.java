6953d42b8e2389b9b16eda4f49b9378b2533d011
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-10-30 19:00:21 +0330

    Add AuthenticationControllerTest

---
+++ Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,20 @@
+package mizdooni.controllers;
+
+import mizdooni.service.UserService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(AuthenticationController.class)
+public class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+}

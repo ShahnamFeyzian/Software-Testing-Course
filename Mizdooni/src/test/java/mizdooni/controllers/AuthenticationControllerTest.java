00e06db8b34b0da89a93dff61e5ee75f4909aa70
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-10-30 19:47:43 +0330

    state: for moving to another PC

--- Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
@@ -1,12 +1,19 @@
 package mizdooni.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mizdooni.model.User;
 import mizdooni.service.UserService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(AuthenticationController.class)
@@ -17,4 +24,15 @@ public class AuthenticationControllerTest {
 
     @MockBean
     private UserService userService;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void getCurrentUser_NoOneLoggedIn_UnAuthorized() throws Exception {
+        when(userService.getCurrentUser()).thenReturn(null);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/user")).andReturn().getResponse();
+
+        User result = mapper.readValue(response.getContentAsString(), User.class);
+    }
 }

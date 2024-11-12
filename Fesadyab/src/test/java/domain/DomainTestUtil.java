a59c781635bbb4bda2302003abee3abc3bc33218
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-11-13 01:33:58 +0330

    Add DomainTestUtil

---
+++ Fesadyab/src/test/java/domain/DomainTestUtil.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class DomainTestUtil {
+    public static int TRANSACTION_ID = 1;
+    public static int ACCOUNT_ID = 1;
+    public static int AMOUNT = 1000;
+    public static Transaction createTransaction() {
+        Transaction transaction = new Transaction();
+        transaction.transactionId = TRANSACTION_ID;
+        transaction.accountId = ACCOUNT_ID;
+        transaction.amount = AMOUNT;
+        transaction.isDebit = false;
+        return transaction;
+    }
+    public static Transaction createDebitTransaction() {
+        Transaction transaction = createTransaction();
+        transaction.isDebit = true;
+        return transaction;
+    }
+    public static Transaction createTransactionWithId(int id) {
+        Transaction transaction = createTransaction();;
+        transaction.transactionId = id;
+        return transaction;
+    }
+    public static Transaction createDebitTransactionWithId(int id) {
+        Transaction transaction = createDebitTransaction();
+        transaction.transactionId = id;
+        return transaction;
+    }
+}
